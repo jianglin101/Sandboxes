883387ea867472e5664a8def062aaa4aae3fbc83
Jianglin Wu <jianglin101@gmail.com>
2019-01-29 21:37:33 -0500

    fix confilict

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-<<<<<<< HEAD
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
@@ -10,11 +9,6 @@ function AppDemo() {
       <App />
     </BrowserRouter>
   );
-=======
-import App from "./App";
-function AppDemo() {
-  return <App />;
->>>>>>> 8fbf78535f553d3b2456b4e42672cbde38af183b
 }
 
 ReactDOM.render(<AppDemo />, document.querySelector("#app"));
